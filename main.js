9a29675b55ba14c0c611ef4bbc0fa6bcfe3a1e96
SamuelDavis2 <samueldavis531@gmail.com>
2026-04-23 23:47:10 +1000

    Moved results from console to screen

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 //Maths functions
 function add(num1, num2) {
-    console.log(num1 + num2);
+    const result = num1 + num2;
+    answer.textContent = result;
 }
 
 function subtract(num1, num2) {
-    console.log(num1 - num2);
+    const result = num1 - num2;
+    answer.textContent = result;
 }
 
 function multiply(num1, num2) {
-    console.log(num1 * num2);
+    const result = num1 * num2;
+    answer.textContent = result;
 }
 
 function divide(num1, num2) {
-    console.log(num1 / num2);
+    const result = num1 / num2;
+    answer.textContent = result;
 }
 
 let num1 = 0;
