752b0abfdb72e356c8536ec21ae37dcfd0a19a4f
SamuelDavis2 <samueldavis531@gmail.com>
2026-04-23 19:53:11 +1000

    Adding button functionality

--- main.js
+++ main.js
@@ -31,3 +31,60 @@ function operate(num1, operator, num2) {
         return 'ERROR: Invalid Operator';
     }
 }
+
+const answer = document.querySelector(".answer");
+const clear = document.querySelector(".clear");
+
+clear.addEventListener("click", () => answer.textContent = '');
+
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+
+one.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`1`);
+    num1 = Number(answer.textContent);
+});
+two.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`2`);
+    num1 = Number(answer.textContent);
+});
+three.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`3`);
+    num1 = Number(answer.textContent);
+});
+four.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`4`);
+    num1 = Number(answer.textContent);
+});
+five.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`5`);
+    num1 = Number(answer.textContent);
+});
+six.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`6`);
+    num1 = Number(answer.textContent);
+});
+seven.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`7`);
+    num1 = Number(answer.textContent);
+});
+eight.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`8`);
+    num1 = Number(answer.textContent);
+});
+nine.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`9`);
+    num1 = Number(answer.textContent);
+});
+zero.addEventListener("click", () => {
+    answer.textContent = answer.textContent.concat(`0`);
+    num1 = Number(answer.textContent);
+});
